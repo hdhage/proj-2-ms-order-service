af51b72ead34625e943fc9380a41f21762274897
Hemant D <hemantdhage20@gmail.com>
2021-08-25 19:14:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# proj-2-ms-order-service
---
+++ src/main/java/com/orderservice/Proj2MsOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj2MsOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj2MsOrderServiceApplication.class, args);
+	}
+
+}
